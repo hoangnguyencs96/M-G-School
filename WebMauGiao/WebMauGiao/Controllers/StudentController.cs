b5f08abe4f6f542cadab2bedd69453e80eaf097d
huygiangit96 <32055253+huygiangit96@users.noreply.github.com>
2018-06-17 18:50:19 +0700

    3 form đầu

---
+++ WebMauGiao/WebMauGiao/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMauGiao.Controllers
+{
+    public class StudentController : SecurityController
+    {
+        // GET: Student
+        public ActionResult ListStudent()
+        {
+            return View();
+        }
+        public ActionResult RollCall()
+        {
+            return View();
+        }
+        public ActionResult ClassArranger()
+        {
+            return View();
+        }
+    }
+}
